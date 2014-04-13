2938a0d9200224b83a75fecd7e0e92085ebbc3d7
Aziz Batihk <aziz@azizmb.com>
2014-04-13 14:34:17 -0400

    Initial Binding
    Initial push. Compiles and runs. /Most/ Swipe triggers work.

---
+++ MCSwipe/StructsAndEnums.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MonoTouch.UIKit;
+using MonoTouch.Foundation;
+using MonoTouch.ObjCRuntime;
+
+namespace MCSwipe
+{
+	public enum MCSwipeTableViewCellState : uint {
+		None = 0,
+		One = 1,
+		Two = 2,
+		Three = 3,
+		Four = 4
+	}
+
+	public enum MCSwipeTableViewCellMode : uint {
+		None = 0,
+		Exit,
+		Switch
+	}
+
+	public enum MCSwipeCompletionBlock : uint {
+		TableViewCellModeNone = 0,
+		TableViewCellModeExit,
+		TableViewCellModeSwitch
+	}
+}
+
