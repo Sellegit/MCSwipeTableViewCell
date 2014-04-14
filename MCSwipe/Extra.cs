3890a3f5a6a984c02b97ebfa17d02835dfd601e8
Aziz Batihk <aziz@azizmb.com>
2014-04-13 23:11:53 -0400

    Created Extra.cs

---
+++ MCSwipe/Extra.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using MonoTouch.UIKit;
+using MonoTouch.Foundation;
+using MonoTouch.ObjCRuntime;
+
+namespace MCSwipe
+{
+	public static class Colors {
+		public static UIColor Green {
+			get {
+				return UIColor.FromRGB (85, 213, 80);
+			}
+		}
+
+		public static UIColor Red {
+			get {
+				return UIColor.FromRGB (232, 61, 14);
+			}
+		}
+
+		public static UIColor Yellow {
+			get {
+				return UIColor.FromRGB (254, 217, 56);
+			}
+		}
+
+		public static UIColor Brown {
+			get {
+				return UIColor.FromRGB (206, 149, 98);
+			}
+		}
+	}
+}
+
